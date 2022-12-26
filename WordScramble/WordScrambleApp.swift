5c747c8e16e6ecf22290ba9dbdb56efa592c8b74
Yaroslav Grogul <time7bass@gmail.com>
2022-12-26 22:47:09 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ярослав Грогуль on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
